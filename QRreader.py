5ba72b12d4b9a14937aead6914884d7fab1e43f0
Joaquin Popoca <granpoposhca@gmail.com>
2019-03-10 14:51:51 -0700

    Add QR code reader to main program

---
+++ QRreader.py
@@ -0,0 +1,3 @@
+def readInput():
+    line = raw_input()
+    return line
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import readConfig as config
+import QRreader as reader
 
 data = config.readFile('./config.json')
+line = reader.readInput()
 print(data)
+print("--")
+print(line)
