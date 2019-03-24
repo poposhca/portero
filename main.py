05ad53dd02e4db7a3f256660bb6e847a2c82467b
poposhca <jjphed@outlook.com>
2019-03-24 16:33:48 -0600

    Connect to server each time an imput is detected.

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ import socket
 conf = config.readFile('./config.json')
 host = conf['HOST']
 port = int(conf['PORT'])
-s = socket.socket()
-print "Attempting to connect to %s on port %s" % (host, port)
-try:
-    s.connect((host, port))
-    while True:
-        print("Selecciona una opcion:\t1)Codigo QR\n\t2)Teclear codigo")
-        keyReader = keyBoardReader.KeyBoardReader()
-        key = 0
-        while(key != 1 and key != 2):
-            key = keyReader.raedInput()
-            print(key)
-        print "Seleccionaste : " + str(key)
-        print("Esperando entrada: ")
-        data = QRreader.readInput()
+
+while True:
+    print("Selecciona una opcion:\t1)Codigo QR\n\t2)Teclear codigo")
+    keyReader = keyBoardReader.KeyBoardReader()
+    key = 0
+    while(key != 1 and key != 2):
+        key = keyReader.raedInput()
+        print(key)
+    print "Seleccionaste : " + str(key)
+    print("Esperando entrada: ")
+    data = QRreader.readInput()
+    try:
+        s = socket.socket()
+        s.settimeout(3)
+        print "Attempting to connect to host, please wait some seconds"
+        s.connect((host, port))
         s.send(data)
         respond = s.recv(1024)
         try:
@@ -32,7 +34,7 @@ try:
                 print("No valid")
         except:
             print "Data input invalid: %s" % (respond)
-except socket.error, e:
-    print "Connection to %s on port %s failed: %s" % (host, port, e)
-finally:
-    s.close()
+    except socket.error, e:
+        print "Connection to %s on port %s failed: %s" % (host, port, e)
+    finally:
+        s.close()
